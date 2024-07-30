5f242a5ec27252559c64ca71d9ef3683c3b1f242
José Bernal <jbernal.web.dev@gmail.com>
2024-07-29 17:58:19 -0700

    Demonstrate instantiating and OrsChapter object using a Response object.

--- src/js/index.js
+++ src/js/index.js
@@ -4,23 +4,28 @@ import '../css/input.css';
 import { vNode, View } from '@ocdla/view';
 import { OrsChapter } from '@ocdla/ors/src/OrsChapter';
 import HttpClient from '@ocdla/lib-http/HttpClient';
+import Url from '@ocdla/lib-http/Url';
 import App from './App';
 /* eslint-enable */
 
 const $body = document.querySelector('body');
 
 let url = new Url('https://appdev.ocdla.org/books-online/index.php');
-url.addParam('chapter', '3');
+url.buildQuery('chapter', '1');
 
 let req = new Request(url.toString());
 
 let client = new HttpClient();
 
-let resp = await client.fetch(req);
+let resp = await client.send(req);
 
-let chapter = await OrsChapter.fromResponse(resp);
-chapter.parse();
-chapter.injectAnchors();
+let chapter = await OrsChapter.fromResponse(resp).then(chapter => {
+    chapter.parse();
+    chapter.injectAnchors();
+    return chapter;
+});
+
+console.log(chapter);
 
 // let doc = await chapter.load(resp);
 
