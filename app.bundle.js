9e22b43ab85fd0df190baa88e242255edc070615
ocdladefense <ocdladefense@users.noreply.github.com>
2024-09-21 21:59:26 +0000

    deploy: 8d941785e2ff60922bdf2a94842a947d1e905f15

--- app.bundle.js
+++ app.bundle.js
@@ -5574,7 +5574,7 @@ async function refresh() {
 /******/ 	
 /******/ 	/* webpack/runtime/publicPath */
 /******/ 	(() => {
-/******/ 		__webpack_require__.p = "/ors-viewer/";
+/******/ 		__webpack_require__.p = "/";
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/nonce */
